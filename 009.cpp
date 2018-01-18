acf5e6a6442d5e33408b15150c742e9bad67777c
panxie <ftdpanxie@gmail.com>
2018-01-18 21:24:37 +0800

    palindrome number,11504 / 11507 test cases passed.

---
+++ 009.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		int m = 0;
+		int temp = x;
+		if (x>INT_MAX || x<INT_MIN)
+			return 0;
+		else
+			while (temp) {
+				m = 10 * m + temp % 10;
+				temp /= 10;
+			}
+		return (m == x);
+	}
+};
+
+int main() {
+	int x = -123;
+	Solution aa;
+	bool ans = aa.isPalindrome(x);
+	std::cout << ans;
+	return 0;
+}
