ec77d5f348b90f9ad7e8f4f0fedd38697603bcb3
PanXiebit <ftdpanxie@gmail.com>
2019-01-05 09:43:57 +0800

    add explanation

--- dp/120.triangle.cpp
+++ dp/120.triangle.cpp
@@ -17,6 +17,18 @@ Bonus point if you are able to do this using only O(n) extra space, where n is t
 
 */
 
+/* 解题思路：
+ * 自顶向下递归，自下而上迭代。 建立一个 map[][] 二元数组，写出i递推公式。
+ * 也就是 map[i][j] 与 map[i-1][j]、map[i-1][j-1] 之间的关系
+ *
+ * 需要注意的是 j 的范围为 [0, i-1]. 而实际上 j 的范围是 [-1, i] 有两个超出的边界。
+ *
+ * 只需要将两个边界设定为 INT_MAX 即可
+ *
+ *
+ *
+*/
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
