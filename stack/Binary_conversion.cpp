4e99ffc60cb443a3a9781638eb8a0feae4e84c85
PanXiebit <ftdpanxie@gmail.com>
2019-01-09 21:52:54 +0800

    stack 使用, 二进制

---
+++ stack/Binary_conversion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+
+void convert(stack<char>& S, int n, int base){//十进制到base进制的转换，递归版
+		//static char digit[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B","C","D","E","F"};
+		const char digit[] = 	"0123456789BCDEF";
+		if (0 < n){
+				S.push(digit[n % base]);
+				convert (S, n/base, base);
+		}
+}
+
+int main(){
+	stack<char> S;
+	convert(S, 10, 2);
+	char a = S.top();
+	while (!S.empty()){
+			char a = S.top();
+			cout << a <<endl;
+			S.pop();
+	}
+	return 0;
+}
+
+
